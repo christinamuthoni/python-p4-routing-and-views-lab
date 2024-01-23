989ca501039ddc1a0eb0ed37c24473249fc195f1
christine <christine.muriungi@student.moringaschool.com>
2024-01-23 17:47:18 +0300

    four instructions

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -16,11 +16,27 @@ def print_string(hello):
 
 @app.route('/count/<int:num>')
 def count(num):
-    numbers =  [i for i in range(num + 1)]
+    numbers = list(range(num + 1))
+
+    # Convert the list of numbers to a string with commas
+    #numbers_str = ', '.join(map(str, numbers))
+    #numbers_str = str(numbers).split('').join('')
+
     return numbers
-   
+
+@app.route('/math/<parameter>')
+def math():
+    num1 = int(request.args.get('num1'))
+    num2 = int(request.args.get('num2'))
+    operation = request.args.get('operation')
+
+    if operation == '+':
+        result = num1 + num2
+        return str(result)
+    else:
+        return "Invalid operation"
     
     
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5553, debug=True)
