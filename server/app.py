ba8048076a00c9121e8268a23471812f8e622b5d
christine <christine.muriungi@student.moringaschool.com>
2024-01-23 13:03:37 +0300

    count,string and index views

--- server/app.py
+++ server/app.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:hello>')
+def print_string(hello):
+    print (f"{hello}")
+
+    return hello
+
+@app.route('/count/<int:num>')
+def count(num):
+    numbers =  [i for i in range(num + 1)]
+    return numbers
+   
+    
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
